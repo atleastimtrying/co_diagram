bea069e5b4cedc84f75c1683200d66ffc4ad746f
Anders Fisher <atleastimtrying@gmail.com>
2014-09-14 11:13:28 +0100

    style improved

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 //make arrows responsive
-//make fore and background colours toglleable;
-//make nested elements show/hide nicely;
+
+//nested responsivity.. scrollTo??
 
 function draw_arrow(ctx, from_x, from_y, to_x, to_y, color) {
   var head_size = 8;
@@ -70,123 +70,128 @@ function draw_arrow(ctx, from_x, from_y, to_x, to_y, color) {
   ctx.stroke();
   ctx.closePath();
 }
-var colour_1 = '#3F4CFF';
+var width = 1000;
+var height = 1000;
+var colour_1 = '#CC9C1E';
 var colour_2 = '#56CC1E';
 var canvas = document.getElementById("c");
 var context = canvas.getContext("2d");
 context.lineWidth = 1;
 context.textAlign = 'center';
 context.textBaseline = 'middle';
+var draw = function(){
+  context.fillStyle = "white";
+  context.fillRect(0,0,width, height);
+  // 1 
+  draw_arrow(context, 240, 46, 760, 46, colour_1);
 
-// 1 
-draw_arrow(context, 240, 46, 760, 46, colour_1);
+  context.beginPath();
+  context.moveTo(490, 46);
+  context.lineTo(490, 170);
+  context.strokeStyle = colour_1;
+  context.stroke();
+  context.closePath();
 
-context.beginPath();
-context.moveTo(490, 46);
-context.lineTo(490, 190);
-context.strokeStyle = colour_1;
-context.stroke();
-context.closePath();
+  context.fillStyle = colour_1;
+  context.fillText('Principal', 500, 36);
 
-context.fillStyle = colour_1;
-context.fillText('Principal', 500, 36);
+  // 2
+  draw_arrow(context, 760, 66, 240, 66, colour_2);
+  draw_arrow(context, 510, 66, 510, 170, colour_2);
 
-// 2
-draw_arrow(context, 760, 66, 240, 66, colour_2);
-draw_arrow(context, 510, 66, 510, 190, colour_2);
+  context.fillStyle = colour_2;
+  context.fillText('Debt Capital', 630, 76);
 
-context.fillStyle = colour_2;
-context.fillText('Debt Capital', 630, 76);
+  draw_arrow(context, 240, 230, 380, 230, colour_1); // 3
+  context.fillStyle = colour_1;
+  context.fillText('Investment', 310, 220);
 
-draw_arrow(context, 240, 250, 380, 250, colour_1); // 3
-context.fillStyle = colour_1;
-context.fillText('Investment', 310, 240);
+  draw_arrow(context, 380, 250, 240, 250, colour_2); // 4
+  context.fillStyle = colour_2;
+  context.fillText('Net Return', 310, 260);
 
-draw_arrow(context, 380, 270, 240, 270, colour_2); // 4
-context.fillStyle = colour_2;
-context.fillText('Net Return', 310, 280);
+  draw_arrow(context, 610, 230, 760, 230, colour_1); // 5
+  context.fillStyle = colour_1;
+  context.fillText('Investment', 680, 220);
 
-draw_arrow(context, 610, 250, 760, 250, colour_1); // 5
-context.fillStyle = colour_1;
-context.fillText('Investment', 680, 240);
+  draw_arrow(context, 760, 250, 610, 250, colour_2); // 6
+  context.fillStyle = colour_2;
+  context.fillText('Gross Return', 680, 260);
 
-draw_arrow(context, 760, 270, 610, 270, colour_2); // 6
-context.fillStyle = colour_2;
-context.fillText('Gross Return', 680, 280);
+  draw_arrow(context, 490, 300, 490, 370, colour_1); // 7
+  context.fillStyle = colour_1;
+  context.fillText('Fees', 470, 335);
 
-draw_arrow(context, 490, 320, 490, 390, colour_1); // 7
-context.fillStyle = colour_1;
-context.fillText('Fees', 470, 355);
+  draw_arrow(context, 510, 370, 510, 300, colour_2); // 8
+  context.fillStyle = colour_2;
+  context.fillText('Advice & Services', 560, 335);
 
-draw_arrow(context, 510, 390, 510, 320, colour_2); // 8
-context.fillStyle = colour_2;
-context.fillText('Advice & Services', 560, 355);
+  draw_arrow(context, 870, 80, 870, 180, colour_1); // 9
+  context.fillStyle = colour_1;
+  context.fillText('Principal', 840, 135);
 
-draw_arrow(context, 870, 100, 870, 190, colour_1); // 9
-context.fillStyle = colour_1;
-context.fillText('Principal', 840, 145);
+  draw_arrow(context, 890, 180, 890, 80, colour_2); // 10
+  context.fillStyle = colour_2;
+  context.fillText('Dept Capital', 930, 135);
 
-draw_arrow(context, 890, 190, 890, 100, colour_2); // 10
-context.fillStyle = colour_2;
-context.fillText('Dept Capital', 930, 145);
+  draw_arrow(context, 710, 380, 800, 290, colour_1); // 11
+  context.fillStyle = colour_1;
+  context.fillText('Technical', 710, 330);
+  context.fillText('Assistance', 710, 344);
 
-draw_arrow(context, 710, 410, 800, 320, colour_1); // 11
-context.fillStyle = colour_1;
-context.fillText('Technical', 710, 360);
-context.fillText('Assistance', 710, 374);
+  draw_arrow(context, 820, 300, 710, 410, colour_2); // 12
+  context.fillStyle = colour_2;
+  context.fillText('Fees', 775, 370);
 
-draw_arrow(context, 820, 320, 710, 430, colour_2); // 12
-context.fillStyle = colour_2;
-context.fillText('Fees', 775, 390);
+  context.beginPath();
+  context.moveTo(120, 500);
+  context.lineTo(120, 570);
+  context.lineTo(870, 570);
+  context.strokeStyle = colour_1;
+  context.stroke();
+  context.closePath();
+  draw_arrow(context, 870, 570, 870, 300, colour_1); // 13
+  context.fillStyle = colour_1;
+  context.fillText('Direct Investment', 200, 560);
 
-context.beginPath();
-context.moveTo(120, 520);
-context.lineTo(120, 590);
-context.lineTo(870, 590);
-context.strokeStyle = colour_1;
-context.stroke();
-context.closePath();
-draw_arrow(context, 870, 590, 870, 320, colour_1); // 13
-context.fillStyle = colour_1;
-context.fillText('Direct Investment', 200, 580);
+  context.beginPath();
+  context.moveTo(890, 300);
+  context.lineTo(890, 590);
+  context.lineTo(100, 590);
+  context.strokeStyle = colour_2;
+  context.stroke();
+  context.closePath();
+  draw_arrow(context, 100, 590, 100, 500, colour_2); // 14
+  context.fillStyle = colour_2;
+  context.fillText('Investment Returns', 800, 600);
 
-context.beginPath();
-context.moveTo(890, 320);
-context.lineTo(890, 610);
-context.lineTo(100, 610);
-context.strokeStyle = colour_2;
-context.stroke();
-context.closePath();
-draw_arrow(context, 100, 610, 100, 520, colour_2); // 14
-context.fillStyle = colour_2;
-context.fillText('Investment Returns', 800, 620);
+  context.beginPath();
+  context.moveTo(490, 570);
+  context.lineTo(490, 560);
+  context.moveTo(490, 550);
+  context.lineTo(490, 540);
+  context.strokeStyle = colour_1;
+  context.stroke();
+  context.closePath();
+  draw_arrow(context, 490, 530, 490, 510, colour_1); // 15
+  context.fillStyle = colour_1;
+  context.fillText('Fees', 470, 540);
 
-context.beginPath();
-context.moveTo(490, 590);
-context.lineTo(490, 580);
-context.moveTo(490, 570);
-context.lineTo(490, 560);
-context.strokeStyle = colour_1;
-context.stroke();
-context.closePath();
-draw_arrow(context, 490, 550, 490, 530, colour_1); // 15
-context.fillStyle = colour_1;
-context.fillText('Fees', 470, 560);
-
-context.beginPath();
-context.moveTo(510, 530);
-context.lineTo(510, 540);
-context.moveTo(510, 550);
-context.lineTo(510, 560);
-context.moveTo(510, 570);
-context.lineTo(510, 580);
-context.strokeStyle = colour_2;
-context.stroke();
-context.closePath();
-draw_arrow(context, 510, 590, 510, 610, colour_2); // 16
-context.fillStyle = colour_2;
-context.fillText('Advice & Services', 560, 560);
-
+  context.beginPath();
+  context.moveTo(510, 510);
+  context.lineTo(510, 520);
+  context.moveTo(510, 530);
+  context.lineTo(510, 540);
+  context.moveTo(510, 550);
+  context.lineTo(510, 560);
+  context.strokeStyle = colour_2;
+  context.stroke();
+  context.closePath();
+  draw_arrow(context, 510, 570, 510, 590, colour_2); // 16
+  context.fillStyle = colour_2;
+  context.fillText('Advice & Services', 560, 540);
+};
+draw();
 $(function(){
   $('#tooltip, #tooltip-bg').hide();
   var boxes = {
@@ -216,8 +221,33 @@ $(function(){
     $('#tooltip').hide(); 
   };
 
+  $('#investment_toggle').click(function(){
+    if($(this).hasClass('showing')){
+      colour_1 = "white";
+      draw();
+      $(this).removeClass('showing');
+    }else{
+      colour_1 = "#CC9C1E";
+      draw();
+      $(this).addClass('showing');
+    }
+  });
+
+  $('#return_toggle').click(function(){
+    if($(this).hasClass('showing')){
+      colour_2 = "white";
+      draw();
+      $(this).removeClass('showing');
+    }else{
+      colour_2 = "#56CC1E";
+      draw();
+      $(this).addClass('showing');
+    }
+  });
+
   $.each(boxes, function(boxId, names) {
-    var name_string = "";
+    var title = $('#' + boxId).html();
+    var name_string = "<h2>" + title + "</h2>";
     $(names).each(function(index, element){ 
       name_string +=  '<li>' + element + "</li>"; 
     });
